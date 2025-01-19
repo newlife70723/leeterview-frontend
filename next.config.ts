4947a1fe78be43ae844022ea6453418da023aa79
Wei <pkh_wei23@photons.com.tw>
2025-01-19 14:21:47 +0800

    feat: update setting about static

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  assetPrefix: process.env.NODE_ENV === "production" ? "https://cdn.leeterview.net" : "",
 };
+
+
 
 export default nextConfig;
