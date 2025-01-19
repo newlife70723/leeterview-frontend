7d46ab33e7fd41921cd598690fe0c1c56bd90ea2
Wei <pkh_wei23@photons.com.tw>
2025-01-19 14:36:55 +0800

    feat: delete setting about static

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  assetPrefix: process.env.NODE_ENV === "production" ? "https://cdn.leeterview.net" : "",
 };
 
 
